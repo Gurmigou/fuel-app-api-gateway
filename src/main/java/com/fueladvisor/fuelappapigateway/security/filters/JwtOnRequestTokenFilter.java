08eece3794fad25eb4d9c79c8fda7a8cf92623f7
Yehor.Bukhanets <Yehor.Bukhanets@infopulse.com>
2022-05-10 17:06:26 +0300

    test

--- src/main/java/com/fueladvisor/fuelappapigateway/security/filters/JwtOnRequestTokenFilter.java
+++ src/main/java/com/fueladvisor/fuelappapigateway/security/filters/JwtOnRequestTokenFilter.java
@@ -1,53 +1,50 @@
-package com.fueladvisor.fuelappapigateway.security.filters;
-
-import com.fueladvisor.fuelappapigateway.security.service.JwtService;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-@Component
-public class JwtOnRequestTokenFilter extends AbstractGatewayFilterFactory<JwtOnRequestTokenFilter.Config> {
-    private final JwtService jwtService;
-
-    public JwtOnRequestTokenFilter(JwtService jwtService) {
-        super(Config.class);
-        this.jwtService = jwtService;
-    }
-
-    public static class Config {
-    }
-
-    @Override
-    public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> {
-            ServerHttpRequest request = exchange.getRequest();
-
-            if (!request.getHeaders().containsKey("Authorization")) {
-                return onError(exchange, HttpStatus.UNAUTHORIZED);
-            }
-
-            String token = request.getHeaders().getFirst("Authorization");
-
-            if (!jwtService.isTokenValid(token)) {
-                return onError(exchange, HttpStatus.UNAUTHORIZED);
-            }
-
-            return chain.filter(exchange);
-        };
-    }
-
-    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus status) {
-        ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(status);
-
-        return response.setComplete();
-    }
-}
+//package com.fueladvisor.fuelappapigateway.security.filters;
+//
+//import com.fueladvisor.fuelappapigateway.security.service.JwtService;
+//import org.springframework.cloud.gateway.filter.GatewayFilter;
+//import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.server.reactive.ServerHttpRequest;
+//import org.springframework.http.server.reactive.ServerHttpResponse;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//
+//@Component
+//public class JwtOnRequestTokenFilter extends AbstractGatewayFilterFactory<JwtOnRequestTokenFilter.Config> {
+//    private final JwtService jwtService;
+//
+//    public JwtOnRequestTokenFilter(JwtService jwtService) {
+//        super(Config.class);
+//        this.jwtService = jwtService;
+//    }
+//
+//    public static class Config {
+//    }
+//
+//    @Override
+//    public GatewayFilter apply(Config config) {
+//        return (exchange, chain) -> {
+//            ServerHttpRequest request = exchange.getRequest();
+//
+//            if (!request.getHeaders().containsKey("Authorization")) {
+//                return onError(exchange, HttpStatus.UNAUTHORIZED);
+//            }
+//
+//            String token = request.getHeaders().getFirst("Authorization");
+//
+//            if (!jwtService.isTokenValid(token)) {
+//                return onError(exchange, HttpStatus.UNAUTHORIZED);
+//            }
+//
+//            return chain.filter(exchange);
+//        };
+//    }
+//
+//    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus status) {
+//        ServerHttpResponse response = exchange.getResponse();
+//        response.setStatusCode(status);
+//
+//        return response.setComplete();
+//    }
+//}
